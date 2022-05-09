226e9493a9c2c3a4a3e123442cc4636679102985
Abhisek Datta <abhi.datta07@gmail.com>
2022-05-09 20:58:40 +0530

    added

--- demo.py
+++ demo.py
@@ -1,3 +1,19 @@
+from cProfile import label
+from calendar import c
 import pandas as pd
 
 df= pd.read_csv("Iris.csv")
+
+print(df.shape)
+#categorical_features=[feature for feature in df.columns
+                     #if df[feature].dtypes=="O"]
+#print(categorical_features)
+
+def remove_cat(data):
+    categorical_features=[feature for feature in data.columns
+                     if data[feature].dtypes=="O"]
+    print(categorical_features)
+    data.drop(labels=categorical_features,axis=1,inplace=True)
+
+remove_cat(df)
+print(df.shape)
