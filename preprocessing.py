353a5bb91e3b2613b73f0e9a9cdfeb28091f7611
Abhisek Datta <abhi.datta07@gmail.com>
2022-05-09 20:58:29 +0530

    cat remove added

--- preprocessing.py
+++ preprocessing.py
@@ -5,10 +5,19 @@ class Processing:
 
     def __init__(self,data):
         self.data=data
+
+    # checking missing values
     def checking_missing_value(self):
+        
         try:
             missing_value= self.data.isnull().sum()
             return missing_value
         except Exception as e:
             return Exception ()
+    
+    # Remve the catagorical columns 
+
+    def
+
+
     
