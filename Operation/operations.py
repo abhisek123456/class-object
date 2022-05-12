567959dd1b0272dadfe5c64323de86c42871ee30
Abhisek Datta <abhi.datta07@gmail.com>
2022-05-12 22:52:56 +0530

    added sum

---
+++ Operation/operations.py
@@ -0,0 +1,9 @@
+class Operations:
+    
+    def __init__(self):
+        pass
+
+    def sum(self,a,b):
+        c=a+b
+        return c
+
---
+++ Operation/test.py
@@ -0,0 +1,8 @@
+from operations import Operations
+
+
+x=Operations()
+a=x.sum(32,34)
+
+print(a)
+
